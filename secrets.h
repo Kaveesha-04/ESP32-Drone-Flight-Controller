c38ff29757e642b03283ff2121b327f1f1abef55
Kaveesha-04 <161196830+Kaveesha-04@users.noreply.github.com>
2026-01-26 18:32:41 +0530

    Update secrets.h to use placeholder values
    Replaced sensitive information with placeholders for security.

--- secrets.h
+++ secrets.h
@@ -2,8 +2,8 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
-const char* SECRET_SSID = "Kaveesha's iPhone"; 
-const char* SECRET_PASS = "Nimsara@2004";
-const char* SECRET_MQTT_IP = "172.20.10.2";
+const char* SECRET_SSID = "SSID"; 
+const char* SECRET_PASS = "PASSWORD";
+const char* SECRET_MQTT_IP = "172.20.***.***
 
 #endif
