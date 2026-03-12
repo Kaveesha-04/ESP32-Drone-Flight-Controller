593d718c410b899de5cf507613033cfca7db7674
Nimsara Karunarathna <161196830+Kaveesha-04@users.noreply.github.com>
2026-03-12 10:33:16 +0530

    Delete secrets.h

--- secrets.h
+++
@@ -1,9 +0,0 @@
-// secrets.h
-#ifndef SECRETS_H
-#define SECRETS_H
-
-const char* SECRET_SSID = "SSID"; 
-const char* SECRET_PASS = "PASSWORD";
-const char* SECRET_MQTT_IP = "172.20.***.***
-
-#endif
